4cc8c9432efee5cf982e2e9f78e2521d9e268482
thomassuedbroecker <thomas.suedbroecker@de.ibm.com>
2021-05-28 09:07:12 +0200

    update restructure folders

--- CE/quay-build-and-push.sh
+++ CE/quay-build-and-push.sh
@@ -7,7 +7,7 @@ export ROOT_PATH=$(PWD)
 echo "************************************"
 echo " web-app"
 echo "************************************"
-cd $ROOT_PATH/web-app
+cd $ROOT_PATH/code/web-app
 docker login quay.io
 docker build -t "quay.io/$REPOSITORY/web-app-ce:v2" -f Dockerfile.os4-webapp .
 docker push "quay.io/$REPOSITORY/web-app-ce:v2"
@@ -17,7 +17,7 @@ echo ""
 echo "************************************"
 echo " articles"
 echo "************************************"
-cd $ROOT_PATH/articles
+cd $ROOT_PATH/code/articles
 docker login quay.io
 docker build -t "quay.io/$REPOSITORY/articles-ce:v3" -f Dockerfile .
 docker push "quay.io/$REPOSITORY/articles-ce:v3"
@@ -28,7 +28,7 @@ echo "************************************"
 echo " web-api"
 echo "************************************"
 
-cd $ROOT_PATH/web-api
+cd $ROOT_PATH/code/web-api
 docker login quay.io
 docker build -t "quay.io/$REPOSITORY/web-api-ce:v7" -f Dockerfile .
 docker push "quay.io/$REPOSITORY/web-api-ce:v7"
